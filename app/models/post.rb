cef9c20fe8a747ace3fb1a1bb1aaac593c6700b3
fabriond <gabrielbasketball@gmail.com>
2016-05-01 22:12:10 -0300

    Correção dos modelos de usuario, post e comentario

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
 	belongs_to :usuario
+	has_many :comments
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,26 @@
+# == Schema Information
+#
+# Table name: usuarios
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default("0"), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  name                   :string
+#
+
 class Usuario < ActiveRecord::Base
   devise :database_authenticatable, :registerable
   has_many :posts
   has_many :comments
+  validates_presence_of :name, :email, :password, :password_confirmation
 end
